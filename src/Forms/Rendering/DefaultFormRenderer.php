d5914a5d42b2a0797a3269f853e11b50cb4c9543
David Grudl <david@grudl.com>
2014-04-04 00:37:16 +0200

    Revert "DefaultFormRenderer already does not modify the HTML class of controls. Classes are applied only when form is rendered. (BC break)"
    This reverts commit bbc47eeef14389d71a42ffe5949120d677745d5a.

--- src/Forms/Rendering/DefaultFormRenderer.php
+++ src/Forms/Rendering/DefaultFormRenderer.php
@@ -129,6 +129,7 @@ class DefaultFormRenderer extends Nette\Object implements Nette\Forms\IFormRende
 	{
 		if ($this->form !== $form) {
 			$this->form = $form;
+			$this->init();
 		}
 
 		$s = '';
@@ -148,6 +149,27 @@ class DefaultFormRenderer extends Nette\Object implements Nette\Forms\IFormRende
 			$s .= $this->renderEnd();
 		}
 		return $s;
+	}
+
+
+	/**
+	 * Initializes form.
+	 * @return void
+	 */
+	protected function init()
+	{
+		// TODO: only for back compatiblity - remove?
+		$wrapper = & $this->wrappers['control'];
+		foreach ($this->form->getControls() as $control) {
+			if ($control->isRequired() && isset($wrapper['.required'])) {
+				$control->getLabelPrototype()->class($wrapper['.required'], TRUE);
+			}
+
+			$el = $control->getControlPrototype();
+			if ($el->getName() === 'input' && isset($wrapper['.' . $el->type])) {
+				$el->class($wrapper['.' . $el->type], TRUE);
+			}
+		}
 	}
 
 
@@ -380,11 +402,7 @@ class DefaultFormRenderer extends Nette\Object implements Nette\Forms\IFormRende
 				$description = '';
 			}
 
-			$el = $control->getControl();
-			if ($el instanceof Html && $el->getName() === 'input') {
-				$el->class($this->getValue("control .$el->type"), TRUE);
-			}
-			$s[] = $el . $description;
+			$s[] = $control->getControl() . $description;
 		}
 		$pair = $this->getWrapper('pair container');
 		$pair->add($this->renderLabel($control));
@@ -403,9 +421,6 @@ class DefaultFormRenderer extends Nette\Object implements Nette\Forms\IFormRende
 		$label = $control->getLabel();
 		if ($label instanceof Html) {
 			$label->add($suffix);
-			if ($control->isRequired()) {
-				$label->class($this->getValue('control .required'), TRUE);
-			}
 		} elseif ($label != NULL) { // @intentionally ==
 			$label .= $suffix;
 		}
@@ -440,9 +455,6 @@ class DefaultFormRenderer extends Nette\Object implements Nette\Forms\IFormRende
 		}
 
 		$el = $control->getControl();
-		if ($el instanceof Html && $el->getName() === 'input') {
-			$el->class($this->getValue("control .$el->type"), TRUE);
-		}
 		return $body->setHtml($el . $description . $this->renderErrors($control));
 	}
 
