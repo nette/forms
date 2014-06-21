4c0a983f8c50d290740cd2b2477a4236005ce161
uestla <kesspess@gmail.com>
2014-06-21 22:43:08 +0200

    focus the first <input /> element after display toggle

--- src/assets/netteForms.js
+++ src/assets/netteForms.js
@@ -442,7 +442,17 @@ Nette.parseJSON = function(s) {
 Nette.toggle = function(id, visible, srcElement) {
 	var elem = document.getElementById(id);
 	if (elem) {
-		elem.style.display = visible ? '' : 'none';
+		if (visible) {
+			elem.style.display = '';
+
+			var children = elem.getElementsByTagName('input');
+			if (children.length) {
+				children[0].focus();
+			}
+
+		} else {
+			elem.style.display = 'none';
+		}
 	}
 };
 
