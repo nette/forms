8699e02663ae3e8a793f35aebe892148f2ceea03
David Grudl <david@grudl.com>
2014-06-24 18:14:04 +0200

    fixed phpDoc [Closes nette/nette#1418]

--- src/Forms/Controls/BaseControl.php
+++ src/Forms/Controls/BaseControl.php
@@ -242,7 +242,7 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 
 	/**
 	 * Generates control's HTML element.
-	 * @return Nette\Utils\Html
+	 * @return Nette\Utils\Html|string
 	 */
 	public function getControl()
 	{
@@ -261,7 +261,7 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements ICo
 	/**
 	 * Generates label's HTML element.
 	 * @param  string
-	 * @return Nette\Utils\Html
+	 * @return Nette\Utils\Html|string
 	 */
 	public function getLabel($caption = NULL)
 	{
