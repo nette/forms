a6008715333ffee107132edbd52a298bf9653884
Ciki <matula.m@gmail.com>
2015-02-09 18:30:33 +0100

    add webalize() support for ô ä ľ characters

--- src/assets/netteForms.js
+++ src/assets/netteForms.js
@@ -530,4 +530,4 @@ Nette.webalize = function(s) {
 	return res.replace(/[^a-z0-9]+/g, '-').replace(/^-|-$/g, '');
 };
 
-Nette.webalizeTable = {\u00e1: 'a', \u010d: 'c', \u010f: 'd', \u00e9: 'e', \u011b: 'e', \u00ed: 'i', \u0148: 'n', \u00f3: 'o', \u0159: 'r', \u0161: 's', \u0165: 't', \u00fa: 'u', \u016f: 'u', \u00fd: 'y', \u017e: 'z'};
+Nette.webalizeTable = {\u00e1: 'a', \u00e4: 'a', \u010d: 'c', \u010f: 'd', \u00e9: 'e', \u011b: 'e', \u00ed: 'i', \u013e: 'l', \u0148: 'n', \u00f3: 'o', \u00f4: 'o', \u0159: 'r', \u0161: 's', \u0165: 't', \u00fa: 'u', \u016f: 'u', \u00fd: 'y', \u017e: 'z'};
