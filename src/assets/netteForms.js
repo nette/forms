c8faca52f78c1e06ec74e27e980e027da351dc94
David Grudl <david@grudl.com>
2015-04-16 10:35:03 +0200

    netteForms.js: fixed getValue for multi select box

--- src/assets/netteForms.js
+++ src/assets/netteForms.js
@@ -50,9 +50,6 @@ Nette.getValue = function(elem) {
 	} else if (elem.type === 'file') {
 		return elem.files || elem.value;
 
-	} else if (elem.name.match(/\[\]$/)) { // multi element with single option
-		return Nette.getValue([elem]);
-
 	} else if (elem.tagName.toLowerCase() === 'select') {
 		var index = elem.selectedIndex,
 			options = elem.options,
@@ -68,6 +65,9 @@ Nette.getValue = function(elem) {
 			}
 		}
 		return values;
+
+	} else if (elem.name && elem.name.match(/\[\]$/)) { // multi element with single option
+		return Nette.getValue([elem]);
 
 	} else if (elem.type === 'checkbox') {
 		return elem.checked;
