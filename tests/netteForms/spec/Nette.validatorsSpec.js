82795e2d98721898dae4ccdc6ceb7586dc1f9647
David Grudl <david@grudl.com>
2015-09-17 16:25:37 +0200

    netteForms: updated regexp for URL and email validation [Closes nette/nette#1539]

---
+++ tests/netteForms/spec/Nette.validatorsSpec.js
@@ -0,0 +1,49 @@
+describe('Nette.validators', function() {
+
+	it('email', function() {
+		expect(Nette.validators.email(null, null, '')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello@world.cz')).toBe(true);
+		expect(Nette.validators.email(null, null, 'hello@localhost')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello@127.0.0.1')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello@localhost.a0')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello@localhost.0a')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello@l.org')).toBe(true);
+		expect(Nette.validators.email(null, null, 'hello@1.org')).toBe(true);
+		expect(Nette.validators.email(null, null, 'jean.françois@lyotard.fr')).toBe(false);
+		expect(Nette.validators.email(null, null, 'jerzy@kosiński.pl')).toBe(true);
+		expect(Nette.validators.email(null, null, 'péter@esterházy.hu')).toBe(false);
+		expect(Nette.validators.email(null, null, 'hello@1.c0m')).toBe(true);
+		expect(Nette.validators.email(null, null, 'hello@1.c')).toBe(true);
+	});
+
+
+	it('url', function() {
+		var v = {value: null};
+		expect(Nette.validators.url(null, null, '', v)).toBe(false);
+		expect(Nette.validators.url(null, null, 'hello', v)).toBe(true);
+		expect(v.value === 'http://hello').toBe(true);
+		expect(Nette.validators.url(null, null, 'nette.org', v)).toBe(true);
+		expect(v.value === 'http://nette.org').toBe(true);
+		expect(Nette.validators.url(null, null, 'http://nette.org0', v)).toBe(false);
+		expect(Nette.validators.url(null, null, 'http://nette.0org', v)).toBe(false);
+		expect(Nette.validators.url(null, null, 'http://_nette.org', v)).toBe(false);
+		expect(Nette.validators.url(null, null, 'http://www._nette.org', v)).toBe(false);
+		expect(Nette.validators.url(null, null, 'http://www.ne_tte.org', v)).toBe(false);
+		expect(Nette.validators.url(null, null, 'http://1.org', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://l.org', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://localhost', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://127.0.0.1', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://[::1]', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://[2001:0db8:0000:0000:0000:0000:1428:57AB]', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://nette.org/path', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://nette.org:8080/path', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'https://www.nette.org/path', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'https://example.c0m', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'https://example.l', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://one_two.example.com', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://_.example.com', v)).toBe(true);
+		expect(Nette.validators.url(null, null, 'http://_e_.example.com', v)).toBe(true);
+	});
+
+});
