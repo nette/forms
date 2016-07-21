4dee057febf11eae5bc0344419e1e6c587f254fe
David Grudl <david@grudl.com>
2016-07-21 14:00:23 +0200

    fixed compatibility with PHP 7.1
    https://wiki.php.net/rfc/invalid_strings_in_arithmetic

--- src/Forms/Validator.php
+++ src/Forms/Validator.php
@@ -68,7 +68,7 @@ class Validator extends Nette\Object
 				case 'value': return $withValue ? $rule->control->getValue() : $m[0];
 				default:
 					$args = is_array($rule->arg) ? $rule->arg : array($rule->arg);
-					$i = (int) $m[1] ? $m[1] - 1 : $i + 1;
+					$i = (int) $m[1] ? (int) $m[1] - 1 : $i + 1;
 					return isset($args[$i]) ? ($args[$i] instanceof IControl ? ($withValue ? $args[$i]->getValue() : "%$i") : $args[$i]) : '';
 			}
 		}, $message);
