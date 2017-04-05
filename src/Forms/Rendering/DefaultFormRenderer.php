nette/forms

commit 1b37eaa680c3272c6700bff1bf3ac3bf434f2e72

DefaultFormRenderer: added error message translation (#145)

src/Forms/Rendering/DefaultFormRenderer.php
@@ -215,6 +215,8 @@ class DefaultFormRenderer implements Nette\Forms\IFormRenderer
 	 */
 	public function renderErrors(Nette\Forms\IControl $control = NULL, $own = TRUE)
 	{
+		$translator = $this->form->getTranslator();
+
 		$errors = $control
 			? $control->getErrors()
 			: ($own ? $this->form->getOwnErrors() : $this->form->getErrors());
@@ -229,7 +231,7 @@ class DefaultFormRenderer implements Nette\Forms\IFormRenderer
 			if ($error instanceof IHtmlString) {
 				$item->addHtml($error);
 			} else {
-				$item->setText($error);
+				$item->setText($translator ? $translator->translate($error) : $error);
 			}
 			$container->addHtml($item);
 		}

commit 64ae3c3ccdba1c20515c1ca4f8d997d693f09d65

Fix for calling renderErrors() from renderPair() which was caused by #145 (->form = NULL)

src/Forms/Rendering/DefaultFormRenderer.php
@@ -215,7 +215,9 @@ class DefaultFormRenderer implements Nette\Forms\IFormRenderer
 	 */
 	public function renderErrors(Nette\Forms\IControl $control = NULL, $own = TRUE)
 	{
-		$translator = $this->form->getTranslator();
+		$translator = $control
+			? ($control instanceof \Nette\Forms\Controls\BaseControl ? $control->getTranslator() : NULL)
+			: $this->form->getTranslator();
 
 		$errors = $control
 			? $control->getErrors()