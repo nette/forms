3d97df71be6b9a2e87e55944d9364517465c9b05
David Grudl <david@grudl.com>
2017-06-15 13:02:34 +0200

    SelectBox: only one item can be rendered as selected at the same time

--- src/Forms/Helpers.php
+++ src/Forms/Helpers.php
@@ -201,6 +201,9 @@ class Helpers
 						. htmlspecialchars((string) $caption, ENT_NOQUOTES, 'UTF-8')
 						. '</option>';
 				}
+				if ($selected === $value) {
+					unset($optionAttrs['selected'], $option->attrs['selected']);
+				}
 			}
 			$res .= $tmp;
 			$tmp = '';
