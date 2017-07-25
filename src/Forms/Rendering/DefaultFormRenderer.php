d66a2ee4a49ffc5e57628ee574101b4a81bb88e1
lachtan <xhrabec@gmail.com>
2017-07-25 18:22:02 +0200

    Fixed DefaultFormRenderer::renderErrors not accepting every instance of IHtmlString

--- src/Forms/Rendering/DefaultFormRenderer.php
+++ src/Forms/Rendering/DefaultFormRenderer.php
@@ -226,8 +226,10 @@ class DefaultFormRenderer implements Nette\Forms\IFormRenderer
 
 		foreach ($errors as $error) {
 			$item = clone $item;
-			if ($error instanceof IHtmlString) {
+			if ($error instanceof Html) {
 				$item->addHtml($error);
+			} elseif ($error instanceof IHtmlString) {
+				$item->addHtml((string) $error);
 			} else {
 				$item->setText($translator ? $translator->translate($error) : $error);
 			}
