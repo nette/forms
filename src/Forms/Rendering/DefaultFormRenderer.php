7b4b5252336c585063bb7c0d9218fd41f46b5254
David Grudl <david@grudl.com>
2017-08-17 13:32:14 +0200

    Revert "DefaultFormRenderer: added error message translation (#145)" [Closes #162]
    This reverts commit 7c4e4ca2c4533022635fa97b7d4637b7456f1618.

--- src/Forms/Rendering/DefaultFormRenderer.php
+++ src/Forms/Rendering/DefaultFormRenderer.php
@@ -214,10 +214,6 @@ class DefaultFormRenderer implements Nette\Forms\IFormRenderer
 	 */
 	public function renderErrors(Nette\Forms\IControl $control = null, $own = true)
 	{
-		$translator = $control
-			? ($control instanceof \Nette\Forms\Controls\BaseControl ? $control->getTranslator() : null)
-			: $this->form->getTranslator();
-
 		$errors = $control
 			? $control->getErrors()
 			: ($own ? $this->form->getOwnErrors() : $this->form->getErrors());
@@ -232,7 +228,7 @@ class DefaultFormRenderer implements Nette\Forms\IFormRenderer
 			if ($error instanceof IHtmlString) {
 				$item->addHtml($error);
 			} else {
-				$item->setText($translator ? $translator->translate($error) : $error);
+				$item->setText($error);
 			}
 			$container->addHtml($item);
 		}
