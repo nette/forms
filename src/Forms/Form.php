4bb5d021448f7a9f929e4e29a8ab7bcc5d2b45a0
Tom Bartoň <grez.cz@gmail.com>
2017-10-06 12:59:00 +0200

    fixed phpDoc (#170)

--- src/Forms/Form.php
+++ src/Forms/Form.php
@@ -353,7 +353,7 @@ class Form extends Container implements Nette\Utils\IHtmlString
 
 	/**
 	 * Tells if the form was submitted.
-	 * @return ISubmitterControl|false  submittor control
+	 * @return ISubmitterControl|bool  submittor control
 	 */
 	public function isSubmitted()
 	{
