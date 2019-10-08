e55a65c6d803f8fb7ae07b3e739a2576b878fc8e
David Grudl <david@grudl.com>
2019-10-08 14:06:41 +0200

    throwing exceptions from __toString() is allowed since PHP 7.4

--- src/Forms/Form.php
+++ src/Forms/Form.php
@@ -619,7 +619,7 @@ class Form extends Container implements Nette\Utils\IHtmlString
 			return $this->getRenderer()->render($this);
 
 		} catch (\Throwable $e) {
-			if (func_num_args()) {
+			if (func_num_args() || PHP_VERSION_ID >= 70400) {
 				throw $e;
 			}
 			trigger_error('Exception in ' . __METHOD__ . "(): {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", E_USER_ERROR);
