b54569f65ec019c0939d491257daa0206beb3fbb
Jan Barášek <janbarasek@gmail.com>
2021-01-06 21:06:53 +0100

    Validator::formatMessage() added validator name to error message  (#263)

--- src/Forms/Validator.php
+++ src/Forms/Validator.php
@@ -61,7 +61,11 @@ class Validator
 			$message = static::$messages[$rule->validator];
 
 		} elseif ($message == null) { // intentionally ==
-			trigger_error("Missing validation message for control '{$rule->control->getName()}'.", E_USER_WARNING);
+			trigger_error(
+				"Missing validation message for control '{$rule->control->getName()}'"
+				. (is_string($rule->validator) ? " (validator '{$rule->validator}')." : '.'),
+				E_USER_WARNING
+			);
 		}
 
 		if ($translator = $rule->control->getForm()->getTranslator()) {
