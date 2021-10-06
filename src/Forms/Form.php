4011fedb077263807520aeb4e0468b00d1a75c1e
David Grudl <david@grudl.com>
2021-10-06 21:44:26 +0200

    removed support for PHP 7

--- src/Forms/Form.php
+++ src/Forms/Form.php
@@ -652,17 +652,8 @@ class Form extends Container implements Nette\HtmlStringable
 	 */
 	public function __toString(): string
 	{
-		try {
-			$this->fireRenderEvents();
-			return $this->getRenderer()->render($this);
-
-		} catch (\Throwable $e) {
-			if (func_num_args() || PHP_VERSION_ID >= 70400) {
-				throw $e;
-			}
-			trigger_error('Exception in ' . __METHOD__ . "(): {$e->getMessage()} in {$e->getFile()}:{$e->getLine()}", E_USER_ERROR);
-			return '';
-		}
+		$this->fireRenderEvents();
+		return $this->getRenderer()->render($this);
 	}
 
 
