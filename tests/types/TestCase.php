7db31898783396241ceb5b6052dbaadfc53d4843
David Grudl <david@grudl.com>
2021-11-09 14:06:07 +0100

    added type inference tests

---
+++ tests/types/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PHPUnit\Framework;
+
+use Tester\Assert;
+
+
+abstract class TestCase extends \Tester\TestCase
+{
+	protected function assertSame(mixed $expected, mixed $actual, string $message = ''): void
+	{
+		Assert::same($expected, $actual, $message);
+	}
+
+
+	protected function assertTrue(mixed $actual, string $message = ''): void
+	{
+		Assert::true($actual, $message);
+	}
+}
