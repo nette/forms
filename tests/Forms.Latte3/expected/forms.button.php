e410aef4344c9c524a63245d991b35645120f86f
David Grudl <david@grudl.com>
2022-12-27 03:06:27 +0100

    Latte: rewritten Runtime as non-static class

--- tests/Forms.Latte3/expected/forms.button.php
+++ tests/Forms.Latte3/expected/forms.button.php
@@ -1,27 +1,27 @@
 <?php
 %A%
-		$form = $this->global->formsStack[] = $this->global->uiControl['myForm'] /* line %d% */;
+		$this->global->forms->begin($form = $this->global->uiControl['myForm']) /* line %d% */;
 		echo '<form';
-		echo Nette\Bridges\FormsLatte\Runtime::renderFormBegin(end($this->global->formsStack), [], false) /* line %d% */;
+		echo $this->global->forms->renderFormBegin([], false) /* line %d% */;
 		echo '>
 	<button';
-		echo ($ʟ_input = Nette\Bridges\FormsLatte\Runtime::item('send', $this->global))->getControlPart()->attributes() /* line %d% */;
+		echo ($ʟ_input = $this->global->forms->item('send'))->getControlPart()->attributes() /* line %d% */;
 		echo '>
 		description of button
 	</button>
 
 	<button';
-		echo ($ʟ_input = Nette\Bridges\FormsLatte\Runtime::item('send', $this->global))->getControlPart()->attributes() /* line %d% */;
+		echo ($ʟ_input = $this->global->forms->item('send'))->getControlPart()->attributes() /* line %d% */;
 		echo '></button>
 
 	<button';
-		echo ($ʟ_input = Nette\Bridges\FormsLatte\Runtime::item('send', $this->global))->getControlPart()->attributes() /* line %d% */;
+		echo ($ʟ_input = $this->global->forms->item('send'))->getControlPart()->attributes() /* line %d% */;
 		echo '>';
 		echo LR\Filters::escapeHtmlText($ʟ_input->getCaption()) /* line %d% */;
 		echo '</button>
 ';
-		echo Nette\Bridges\FormsLatte\Runtime::renderFormEnd(end($this->global->formsStack), false) /* line %d% */;
+		echo $this->global->forms->renderFormEnd(false) /* line %d% */;
 		echo '</form>
 ';
-		array_pop($this->global->formsStack);
+		$this->global->forms->end();
 %A%
