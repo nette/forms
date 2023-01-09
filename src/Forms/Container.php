f32b7efa8228279e35a5bb61dbfde2d7639e0673
David Grudl <david@grudl.com>
2023-01-09 21:57:49 +0100

    added deprecated notices

--- src/Forms/Container.php
+++ src/Forms/Container.php
@@ -182,6 +182,7 @@ class Container extends Nette\ComponentModel\Container implements \ArrayAccess
 	/** @deprecated use getUntrustedValues() */
 	public function getUnsafeValues($returnType, ?array $controls = null)
 	{
+		trigger_error(__METHOD__ . '() was renamed to getUntrustedValues()', E_USER_DEPRECATED);
 		return $this->getUntrustedValues($returnType, $controls);
 	}
 
@@ -566,6 +567,7 @@ class Container extends Nette\ComponentModel\Container implements \ArrayAccess
 	/** @deprecated  use addImageButton() */
 	public function addImage(): Controls\ImageButton
 	{
+		trigger_error(__METHOD__ . '() was renamed to addImageButton()', E_USER_DEPRECATED);
 		return $this->addImageButton(...func_get_args());
 	}
 
