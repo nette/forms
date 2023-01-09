3ef26a40c9c21a53776bd70c7104cebce56fb33a
David Grudl <david@grudl.com>
2023-01-09 21:57:49 +0100

    added deprecated notices

--- src/Forms/Controls/Checkbox.php
+++ src/Forms/Controls/Checkbox.php
@@ -92,6 +92,7 @@ class Checkbox extends BaseControl
 	/** @deprecated  use getContainerPrototype() */
 	public function getSeparatorPrototype(): Html
 	{
+		trigger_error(__METHOD__ . '() was renamed to getContainerPrototype()', E_USER_DEPRECATED);
 		return $this->container;
 	}
 }
