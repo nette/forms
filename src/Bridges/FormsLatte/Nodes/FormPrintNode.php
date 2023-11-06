82706a0018116e66b974e0e8225b17c07295d421
David Grudl <david@grudl.com>
2023-11-06 21:53:51 +0100

    Latte: rewritten Runtime as non-static class

--- src/Bridges/FormsLatte/Nodes/FormPrintNode.php
+++ src/Bridges/FormsLatte/Nodes/FormPrintNode.php
@@ -47,7 +47,7 @@ class FormPrintNode extends StatementNode
 			'Nette\Forms\Blueprint::%raw('
 			. ($this->name
 				? 'is_object($ʟ_tmp = %node) ? $ʟ_tmp : $this->global->uiControl[$ʟ_tmp]'
-				: 'end($this->global->formsStack)')
+				: '$this->global->forms->current()')
 			. ') %2.line; exit;',
 			$this->mode === 'formPrint' ? 'latte' : 'dataClass',
 			$this->name,
