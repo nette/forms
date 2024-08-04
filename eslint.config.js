f430d70f120c170134f597723429816be7fea309
David Grudl <david@grudl.com>
2024-08-04 05:09:06 +0200

    eslint: used @stylistic rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,53 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
 
 export default [
-	pluginJs.configs.recommended,
 	{
-		ignores: ['**/*.min.js'],
+		ignores: [
+			'*/', '!src/', '!tests/',
+			'**/netteForms*.*',
+		],
 	},
+
+	pluginJs.configs.recommended,
+	...tseslint.configs.recommended,
+
+	stylistic.configs.customize({
+		indent: 'tab',
+		braceStyle: '1tbs',
+		arrowParens: true,
+		semi: true,
+		jsx: false,
+	}),
+
 	{
 		languageOptions: {
-			ecmaVersion: 2021,
+			ecmaVersion: 'latest',
 			globals: {
 				...globals.browser,
-				'Tracy': 'writeable',
-				'define': 'readable',
-				'module': 'readable',
+				...globals.jasmine,
+				...globals.amd,
+				...globals.commonjs,
+				Nette: 'readable',
+				Tracy: 'writeable',
 			},
 		},
+		plugins: {
+			'@stylistic': stylistic,
+		},
 		rules: {
-			indent: ['error', 'tab'],
-			quotes: ['error', 'single'],
-			semi: ['error', 'always'],
-			'func-style': ['error', 'declaration', {'allowArrowFunctions': true}],
-			'prefer-arrow-callback': ['error'],
-			'arrow-parens': ['error'],
-			'arrow-spacing': ['error'],
-			'no-var': ['error'],
+			'@stylistic/no-multiple-empty-lines': ['error', { max: 2, maxEOF: 0 }],
+			'@stylistic/new-parens': ['error', 'never'],
+			'@stylistic/padded-blocks': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'func-style': ['error', 'declaration', { allowArrowFunctions: true }],
+			'prefer-arrow-callback': 'error',
+			'arrow-body-style': 'error',
+			'eqeqeq': ['error', 'always', { null: 'ignore' }],
+			'no-var': 'error',
+			'prefer-const': 'off',
 		},
 	},
 ];
