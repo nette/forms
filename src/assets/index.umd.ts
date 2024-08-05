ae11e90098c39576d8744d6bacc35dd858ed4167
David Grudl <david@grudl.com>
2024-08-06 01:34:19 +0200

    netteForms: restructured package, includes UMD and ESM (BC break)

--- src/assets/index.umd.ts
+++ src/assets/index.umd.ts
@@ -1,6 +1,6 @@
 import { FormValidator } from './formValidator';
 import { webalize } from './webalize';
-import { version } from './package.json';
+import { version } from './dist/package.json';
 
 type NetteForms = FormValidator & { version: string; webalize: typeof webalize };
 let nette = new FormValidator as NetteForms;
