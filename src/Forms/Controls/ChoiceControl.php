2efdb7a7e384f2c499853e153668ca7968c819ad
David Grudl <david@grudl.com>
2025-03-31 03:14:00 +0200

    BaseControl::$disabled is bool, added $disabledChoices

--- src/Forms/Controls/ChoiceControl.php
+++ src/Forms/Controls/ChoiceControl.php
@@ -21,6 +21,8 @@ use function array_combine, array_fill_keys, array_key_exists, array_keys, array
  */
 abstract class ChoiceControl extends BaseControl
 {
+	/** @var bool[] */
+	protected array $disabledChoices = [];
 	private bool $checkDefaultValue = true;
 	private array $items = [];
 
@@ -70,7 +72,7 @@ abstract class ChoiceControl extends BaseControl
 	{
 		return $this->value !== null
 			&& array_key_exists($this->value, $this->items)
-			&& !isset($this->disabled[$this->value])
+			&& !isset($this->disabledChoices[$this->value])
 			? $this->value
 			: null;
 	}
@@ -129,12 +131,11 @@ abstract class ChoiceControl extends BaseControl
 	public function setDisabled(bool|array $value = true): static
 	{
 		if (!is_array($value)) {
+			$this->disabledChoices = [];
 			return parent::setDisabled($value);
 		}
-
-		parent::setDisabled(false);
-		$this->disabled = array_fill_keys($value, value: true);
-		return $this;
+		$this->disabledChoices = array_fill_keys($value, value: true);
+		return parent::setDisabled(false);
 	}
 
 
