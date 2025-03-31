f247bd1a49c65cf26180a32ee49ba17d70ea6e5a
David Grudl <david@grudl.com>
2025-03-31 03:14:00 +0200

    BaseControl::$disabled is bool, added $disabledChoices

--- src/Forms/Controls/BaseControl.php
+++ src/Forms/Controls/BaseControl.php
@@ -46,9 +46,7 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements Con
 	protected mixed $value = null;
 	protected Html $control;
 	protected Html $label;
-
-	/** @var bool|bool[] */
-	protected bool|array $disabled = false;
+	protected bool $disabled = false;
 
 	/** @var array<string, array<class-string, callable(static): mixed>> */
 	private static array $extMethods = [];
@@ -193,7 +191,7 @@ abstract class BaseControl extends Nette\ComponentModel\Component implements Con
 	 */
 	public function isDisabled(): bool
 	{
-		return $this->disabled === true;
+		return $this->disabled;
 	}
 
 
