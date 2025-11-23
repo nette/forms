473c259ddcf24c3ca6e9a865df2c775d71395e56
David Grudl <david@grudl.com>
2025-11-23 21:15:21 +0100

    requires Latte 3.1

--- tests/Forms.Latte/expected/forms.get.php
+++ tests/Forms.Latte/expected/forms.get.php
@@ -1,20 +1,20 @@
 <?php
 %A%
-		$form = $this->global->formsStack[] = $this->global->uiControl['myForm'] /* %a% */;
+		$form = $this->global->formsStack[] = $this->global->uiControl['myForm'] /* pos 1:1 */;
 		Nette\Bridges\FormsLatte\Runtime::initializeForm($form);
-		echo Nette\Bridges\FormsLatte\Runtime::renderFormBegin($form, []) /* %a% */;
-		echo Nette\Bridges\FormsLatte\Runtime::renderFormEnd(array_pop($this->global->formsStack)) /* %a% */;
+		echo Nette\Bridges\FormsLatte\Runtime::renderFormBegin($form, []) /* pos 1:1 */;
+		echo Nette\Bridges\FormsLatte\Runtime::renderFormEnd(array_pop($this->global->formsStack)) /* pos 1:1 */;
 
 		echo '
 
 ';
-		$form = $this->global->formsStack[] = $this->global->uiControl['myForm'] /* %a% */;
+		$form = $this->global->formsStack[] = $this->global->uiControl['myForm'] /* pos 3:7 */;
 		Nette\Bridges\FormsLatte\Runtime::initializeForm($form);
 		echo '<form';
-		echo Nette\Bridges\FormsLatte\Runtime::renderFormBegin(end($this->global->formsStack), [], false) /* %a% */;
+		echo Nette\Bridges\FormsLatte\Runtime::renderFormBegin(end($this->global->formsStack), [], false) /* pos 3:7 */;
 		echo '>
 ';
-		echo Nette\Bridges\FormsLatte\Runtime::renderFormEnd(end($this->global->formsStack), false) /* %a% */;
+		echo Nette\Bridges\FormsLatte\Runtime::renderFormEnd(end($this->global->formsStack), false) /* pos 3:7 */;
 		echo '</form>
 ';
 		array_pop($this->global->formsStack);
