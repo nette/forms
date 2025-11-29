2f13b442e497892ccbf7ab3a54eff394380addb0
David Grudl <david@grudl.com>
2025-11-29 12:52:07 +0100

    Container::getControls() does not use deprecated parameters

--- src/Forms/Container.php
+++ src/Forms/Container.php
@@ -299,9 +299,15 @@ class Container extends Nette\ComponentModel\Container implements \ArrayAccess
 	 * Iterates over all form controls.
 	 * @return iterable<Control>
 	 */
-	public function getControls(): \Iterator
+	public function getControls(): iterable
 	{
-		return $this->getComponents(true, Control::class);
+		return Nette\Utils\Iterables::repeatable(function () {
+			foreach ($this->getComponentTree() as $component) {
+				if ($component instanceof Control) {
+					yield $component->getName() => $component;
+				}
+			}
+		});
 	}
 
 
